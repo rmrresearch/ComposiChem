9a087cd3c8eb55c836cc0738f7e836be562f3ce9
Ryan M. Richard <rrichard@ameslab.gov>
2024-11-15 10:09:03 -0600

    no ci/cd for now

--- cmake/get_nwx_cmake.cmake
+++ cmake/get_nwx_cmake.cmake
@@ -1,4 +1,4 @@
-# Copyright 2024 NWChemEx-Project
+# Copyright 2024 Richard Group
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
